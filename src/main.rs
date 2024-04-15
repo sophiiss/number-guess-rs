b67a13be304204d9a27afdb7f60b264cee152fbc
sophia <sophialeal@proton.me>
2024-04-15 19:24:37 -0300

    add input parser and rng

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use rand::{rngs::ThreadRng, Rng};
+use std::io::{stdin, stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+
+    let random_number = generate_number(&mut rng);
+    println!("{}, {}", parse_u8(), random_number);
+}
+
+fn generate_number(rng: &mut ThreadRng) -> u8 {
+    let range: std::ops::RangeInclusive<u8> = 1..=10;
+    rng.gen_range(range)
+}
+
+fn read_input() -> String {
+    let mut buffer = String::new();
+    loop {
+        match stdin().read_line(&mut buffer) {
+            Ok(_) => return buffer.trim().to_owned(),
+            Err(_) => {
+                print!("Invalid input. Try again: ");
+                stdout().flush().unwrap();
+                continue;
+            }
+        }
+    }
+}
+
+fn parse_u8() -> u8 {
+    loop {
+        let input = read_input();
+        match input.parse::<u8>() {
+            Ok(num) => break num,
+            Err(_) => {
+                print!("Invalid input. Try again: ");
+                stdout().flush().unwrap();
+                continue;
+            }
+        }
+    }
 }
